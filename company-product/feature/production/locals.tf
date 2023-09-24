3104d84fa9def1b1c020f532d55ff16b20ecf147
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-23 19:00:52 -0500

    rename

--- company-product/feature/production/locals.tf
+++ company-product/feature/production/locals.tf
@@ -1,7 +1,7 @@
 locals {
   region = "East US"
   resource_group_name = "rg-production-tfpoc-feature"
-  cosmosdb_name = "cbd-production-tfpoc-feature"
+  cosmosdb_name = "cdb-production-tfpoc-feature"
   secret_namespace = "production"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-production-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-production-tfpoc-bootstrap"
 }
