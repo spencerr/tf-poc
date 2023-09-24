96183f6bb77f1d6eb877260374615a72347541f4
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-23 21:21:01 -0500

    rename

--- bootstrap/production/locals.tf
+++ bootstrap/production/locals.tf
@@ -3,5 +3,5 @@ locals {
   subscription_id = "8f09f7f4-7b25-4d6f-88a5-847b1751c4ce"
   region = "East US"
   resource_group_name = "rg-prod-tfpoc-bootstrap"
-  key_vault_name = "kv-production-tfpoc-bootstrap"
+  key_vault_name = "kv-prod-tfpoc-bootstrap"
 }
