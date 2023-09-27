7f5cc3c0a66eec56501634a9a0c9746eb0c99216
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-26 22:30:40 -0500

    simplify keyvault

--- modules/keyvault/keyvault.tf
+++ modules/keyvault/keyvault.tf
@@ -1,9 +1,12 @@
+data "azurerm_client_config" "current" {}
+
+
 resource "azurerm_key_vault" "vault" {
   name                        = var.name
   location                    = var.location
   resource_group_name         = var.resource_group_name
   sku_name                    = "standard"  # You can use "premium" for a premium SKU
-  tenant_id                   = var.tenant_id
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
   enabled_for_deployment      = true
   enabled_for_disk_encryption = true
   enabled_for_template_deployment = true
