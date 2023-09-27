dd6ee396aa869783e37f6b1eb5c768cfd021bf55
spencerr <spencerritzow@gmail.com>
2023-09-27 16:50:55 -0500

    update beta/preview feature to 0.0.10 (#40)

--- company-product/domains/feature/preview/main.tf
+++ company-product/domains/feature/preview/main.tf
@@ -1,10 +1,11 @@
 module "feature" {
-  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.7"
+  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.10"
 
   cosmosdb            = local.cosmosdb
   key_vault_name      = local.key_vault_name
   region              = local.region
   resource_group_name = local.resource_group_name
   secret_namespace    = local.secret_namespace
+  storage_account_name = local.storage_account_name
   test_secret_key_vault_id = local.test_secret_key_vault_id
 }
