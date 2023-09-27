00e53e8efe8fdf358dd01e533720c6c126745ad9
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-26 21:56:37 -0500

    rename

--- bootstrap/sandbox/locals.tf
+++ bootstrap/sandbox/locals.tf
@@ -3,5 +3,5 @@ locals {
   subscription_id = "8f09f7f4-7b25-4d6f-88a5-847b1751c4ce"
   region = "East US"
   resource_group_name = "rg-sandbox-tfpoc-bootstrap"
-  key_vault_name = "kv-sandbox-tfpoc-bootstrap"
+  key_vault_name = "kv-sand-tfpoc-bootstrap"
 }
--- company-product/domains/feature/sandbox/locals.tf
+++ company-product/domains/feature/sandbox/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  key_vault_name = "kv-sandbox-tfpoc-feature"
+  key_vault_name = "kv-sand-tfpoc-feature"
   region = "East US"
   resource_group_name = "rg-sandbox-tfpoc-feature"
   secret_namespace = "sandbox"
-  test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-sandbox-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-sandbox-tfpoc-bootstrap"
+  test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-sandbox-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-sand-tfpoc-bootstrap"
   redis = {
     capacity = 2
     enabled = true
