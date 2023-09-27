ded65badae4207bfabd9460076b1869cba16ded1
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-27 16:43:40 -0500

    update beta/preview feature to 0.0.10

--- company-product/domains/feature/beta/locals.tf
+++ company-product/domains/feature/beta/locals.tf
@@ -8,5 +8,6 @@ locals {
   region = "East US"
   resource_group_name = "rg-beta-tfpoc-feature"
   secret_namespace = "beta"
+  storage_account_name = "sabetafeature"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-beta-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-beta-tfpoc-bootstrap"
 }
