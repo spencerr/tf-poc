1dadbc080c6a9ff50ca4db4ab77c4cec7edf2af4
spencerr <spencerritzow@gmail.com>
2023-09-26 23:43:39 -0500

    use 0.0.7 feature in alpha (#30)
    * use 0.0.5 feature in alpha
    
    * Update main.tf
    
    * update workflow

--- company-product/domains/feature/alpha/locals.tf
+++ company-product/domains/feature/alpha/locals.tf
@@ -1,8 +1,13 @@
 locals {
-  cosmosdb_name = "cdb-alpha-tfpoc-feature"
   key_vault_name = "kv-alpha-tfpoc-feature"
   region = "East US"
   resource_group_name = "rg-alpha-tfpoc-feature"
   secret_namespace = "alpha"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-alpha-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-alpha-tfpoc-bootstrap"
+  redis = {
+    capacity = 2
+    enabled = true
+    name = "rc-alpha-tfpoc-feature"
+    sku_name = "Standard"
+  }
 }
--- company-product/domains/feature/alpha/main.tf
+++ company-product/domains/feature/alpha/main.tf
@@ -1,8 +1,8 @@
 module "feature" {
-  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.2"
+  source = "../module"
 
-  cosmosdb_name       = local.cosmosdb_name
   key_vault_name      = local.key_vault_name
+  redis               = local.redis
   region              = local.region
   resource_group_name = local.resource_group_name
   secret_namespace    = local.secret_namespace
