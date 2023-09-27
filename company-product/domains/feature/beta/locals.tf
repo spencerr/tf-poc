7b0bdee89a0d13d54645264b76a8be9f13ab908a
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-26 23:46:51 -0500

    promote 0.0.7 to beta/preview

--- company-product/domains/feature/beta/locals.tf
+++ company-product/domains/feature/beta/locals.tf
@@ -1,8 +1,12 @@
 locals {
-  cosmosdb_name = "cdb-beta-tfpoc-feature"
   key_vault_name = "kv-beta-tfpoc-feature"
   region = "East US"
   resource_group_name = "rg-beta-tfpoc-feature"
   secret_namespace = "beta"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-beta-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-beta-tfpoc-bootstrap"
+  cosmosdb = {
+    enabled = true
+    name = "cos-beta-tfpoc-feature"
+    sku_name = "Standard"
+  }
 }
--- company-product/domains/feature/preview/locals.tf
+++ company-product/domains/feature/preview/locals.tf
@@ -1,8 +1,12 @@
 locals {
-  cosmosdb_name = "cdb-preview-tfpoc-feature"
   key_vault_name = "kv-preview-tfpoc-feature"
   region = "East US"
   resource_group_name = "rg-preview-tfpoc-feature"
   secret_namespace = "preview"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-beta-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-beta-tfpoc-bootstrap"
+  cosmosdb = {
+    enabled = true
+    name = "cos-preview-tfpoc-feature"
+    sku_name = "Standard"
+  }
 }
