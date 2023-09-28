346745ca48be99f560e1b4b5910987879a0aa2b3
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-28 16:56:10 -0500

    0.0.13

--- company-product/domains/feature/beta/main.tf
+++ company-product/domains/feature/beta/main.tf
@@ -1,5 +1,5 @@
 module "feature" {
-  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.12"
+  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.13"
 
   cosmosdb            = local.cosmosdb
   key_vault_name      = local.key_vault_name
