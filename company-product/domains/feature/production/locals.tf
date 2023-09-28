a7ebcaad6359a563a4399bd05ad3815ad2985846
Spencer Ritzow <spencerritzow@gmail.com>
2023-09-28 10:53:36 -0500

    update prod feature to 0.0.10

--- company-product/domains/feature/production/locals.tf
+++ company-product/domains/feature/production/locals.tf
@@ -8,5 +8,6 @@ locals {
   region = "East US"
   resource_group_name = "rg-tfpoc-feature"
   secret_namespace = "production"
+  storage_account_name = "saprodfeature"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-prod-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-prod-tfpoc-bootstrap"
 }
