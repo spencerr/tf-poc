950f8775b26277bf2ddad65b88ce264b735bd9f5
spencerr <spencerritzow@gmail.com>
2023-09-29 09:43:03 -0500

    feature 0.0.13 beta/preview (#53)
    * feature 0.0.11 beta/preview
    
    * update module
    
    * update provider
    
    * Update feature.tf
    
    * 0.0.13
    
    * Update main.tf
    
    * Update main.tf

--- company-product/domains/feature/preview/locals.tf
+++ company-product/domains/feature/preview/locals.tf
@@ -10,4 +10,9 @@ locals {
   secret_namespace = "preview"
   storage_account_name = "sapreviewfeature"
   test_secret_key_vault_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-beta-tfpoc-bootstrap/providers/Microsoft.KeyVault/vaults/kv-beta-tfpoc-bootstrap"
+  app_insights = {
+    name = "ai-tfpoc-feature"
+    application_type = "web"
+    log_analytics_workspace_id = "/subscriptions/8f09f7f4-7b25-4d6f-88a5-847b1751c4ce/resourceGroups/rg-sandbox-tfpoc-bootstrap/providers/Microsoft.OperationalInsights/workspaces/la-sandbox-tfpoc"
+  }
 }
--- company-product/domains/feature/preview/main.tf
+++ company-product/domains/feature/preview/main.tf
@@ -1,5 +1,5 @@
 module "feature" {
-  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.10"
+  source = "git::https://github.com/spencerr/tf-poc//company-product/domains/feature/module/?ref=company-product-v0.0.13"
 
   cosmosdb            = local.cosmosdb
   key_vault_name      = local.key_vault_name
@@ -8,4 +8,5 @@ module "feature" {
   secret_namespace    = local.secret_namespace
   storage_account_name = local.storage_account_name
   test_secret_key_vault_id = local.test_secret_key_vault_id
+  app_insights = local.app_insights
 }
